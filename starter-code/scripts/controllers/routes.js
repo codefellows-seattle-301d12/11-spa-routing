01f7d7023373bf9858227a251c685a2fbfeb0e09
ackenepah <ackenepah@gmail.com>
2016-09-26 16:21:08 -0700

    Used our DOM skills to fade in articles and about content.

--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -2,6 +2,6 @@ page();
 /* DONE: Configure our two routes for this app with page.js,
     by registering each URL your app can handle, linked to a
     single controller function to handle it: */
-page('/', goHome());
-page('/about', goAbout());
+page('/', articleController.reveal());
+page('/about', aboutController.reveal());
 // DONE: What function do we call to activate page.js?
