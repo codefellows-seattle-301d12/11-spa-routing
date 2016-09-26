d5459e905522c60f78ab2db171a7b4023129596b
Bhavya Bharti <bhavyab@outlook.com>
2016-09-26 16:03:57 -0700

    invoked page for two routs

--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -1,5 +1,7 @@
-/* TODO: Configure our two routes for this app with page.js,
+page();
+/* DONE: Configure our two routes for this app with page.js,
     by registering each URL your app can handle, linked to a
     single controller function to handle it: */
-
+page('/', goHome());
+page('/about', goAbout());
 // TODO: What function do we call to activate page.js?
